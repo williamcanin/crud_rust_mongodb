7a1b265f295f7c2798fa94e8c34a0cd77b3567b5
William C. Canin <william.costa.canin@gmail.com>
2024-08-31 21:20:05 -0300

    Update

--- example3/src/users/model.rs
+++ example3/src/users/model.rs
@@ -1,23 +1,3 @@
-// use mongodb::{bson::doc, options::{ClientOptions, ServerApi, ServerApiVersion}, Client};
-// #[tokio::main]
-// async fn main() -> mongodb::error::Result<()> {
-//   let mut client_options =
-//     ClientOptions::parse("mongodb+srv://<username>:<db_password>@cluster0.ofbyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-//       $.await?;
-//   // Set the server_api field of the client_options object to set the version of the Stable API on the client
-//   let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
-//   client_options.server_api = Some(server_api);
-//   // Get a handle to the cluster
-//   let client = Client::with_options(client_options)?;
-//   // Ping the server to see if you can connect to the cluster
-//   client
-//     .database("admin")
-//     .run_command(doc! {"ping": 1}, None)
-//     .await?;
-//   println!("Pinged your deployment. You successfully connected to MongoDB!");
-//   Ok(())
-// }
-
 use mongodb::{Client, Collection, Database};
 use serde::{Deserialize, Serialize};
 
